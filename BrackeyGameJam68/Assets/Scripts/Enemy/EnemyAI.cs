894942a55afcd992a86b1a49d37af763e2e477d4
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-08-27 22:42:22 +0700

    chore: add TODO to `EnemyAI` so i dont forget

--- BrackeyGameJam68/Assets/Scripts/Enemy/EnemyAI.cs
+++ BrackeyGameJam68/Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+// TODO: When doing kill/die method, also call the `RemoveByKey` method in the respective EnemyTracker -mistertfy64 2025-08-27
+
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] public Transform target;
