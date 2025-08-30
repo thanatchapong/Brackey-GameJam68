4fd85292d24addeb9928588c7876f0e7c8733218
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-08-31 01:10:13 +0700

    feat: ranged enemies now "fire" bullets if player approaches it close enough

---
+++ BrackeyGameJam68/Assets/Scripts/Enemy/BulletEnemyNavigation.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class BulletEnemyNavigation : MonoBehaviour
+{
+  [SerializeField] public Transform target;
+  [SerializeField] public int id;
+
+  private UnityEngine.AI.NavMeshAgent agent;
+
+  private void Start()
+  {
+    agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    agent.updateRotation = false;
+    agent.updateUpAxis = false;
+
+    agent.avoidancePriority = Random.Range(30, 60);
+    agent.obstacleAvoidanceType = (UnityEngine.AI.ObstacleAvoidanceType)2;
+    agent.radius = 0.3f;
+    agent.stoppingDistance = 0.5f;
+
+    if (target != null)
+    {
+      agent.SetDestination(target.position);
+    }
+  }
+
+  private void Update()
+  {
+
+  }
+
+  private void OnCollisionStay(Collision collision)
+  {
+    if (collision.gameObject.CompareTag("Enemy"))
+    {
+      Vector3 dir = (transform.position - collision.transform.position).normalized;
+      agent.Move(dir * 0.05f);
+    }
+  }
+}
---
+++ BrackeyGameJam68/Assets/Scripts/Enemy/RangedEnemyAttacking.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class RangedEnemyAttacking : MonoBehaviour
+{
+
+  [SerializeField] public Transform target;
+  private float distanceToFire = 3.75f;
+
+  GameObject player, bulletEnemyPrefab;
+
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+  {
+    bulletEnemyPrefab = Resources.Load<GameObject>("BulletEnemy");
+    player = GameObject.FindGameObjectWithTag("Player");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+    float squaredDistanceFromTarget = (transform.position.x - player.transform.position.x) * (transform.position.x - player.transform.position.x) + (transform.position.y - player.transform.position.y) * (transform.position.y - player.transform.position.y);
+    Debug.Log("DISTANCE: " + squaredDistanceFromTarget);
+    if (squaredDistanceFromTarget < distanceToFire * distanceToFire)
+    {
+      // fire bullets because the enemy is close enough to the player.
+      GameObject bulletEnemy = Instantiate(bulletEnemyPrefab, transform.position, transform.rotation);
+      bulletEnemy.GetComponent<EnemyAI>().target = player.transform;
+    }
+  }
+}
+
